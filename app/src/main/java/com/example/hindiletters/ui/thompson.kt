495b3f097379ffaffcadbd6113b6969d7c262845
salman <aaseem.mhaskar.am@gmail.com>
2023-01-03 14:20:17 +0530

    changes made

--- app/src/main/java/com/example/hindiletters/ui/thompson.kt
+++ app/src/main/java/com/example/hindiletters/ui/thompson.kt
@@ -5,18 +5,19 @@ import java.util.Random
 fun generateRandomInt(min: Int, max: Int): Int {
     return Random().nextInt(max - min + 1) + min
 }
-
+//for level 1
 fun generateRandomHindiLetter(): String {
     val hindiLetters = "अआइईउऊएकखगघचजटठडतदधनपबमयरलवशसह"
     val index = generateRandomInt(0, hindiLetters.length - 1)
     return hindiLetters[index].toString()
 }
-
+// for level 2
 fun generateRandomHindiLetter2(): String {
     val hindiLetters = "ऋऌऍऎऐऑऒओऔङछझञढणथफभऱळऴष"
     val index = generateRandomInt(0, hindiLetters.length - 1)
     return hindiLetters[index].toString()
 }
+
 
 fun main(){
 
